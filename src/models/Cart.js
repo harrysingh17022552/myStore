d472287657101d87cdef00def13226063e07c078
Harish Nigam <harrysingh17022552@gmail.com>
2025-12-23 16:04:51 +0530

    Update Cart.js
    Updating cart schema validation as per our requirement

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 const cartSchema = mongoose.Schema({
-  product_id: { type: Object, required: true },
+  product_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "products", //fk
+    required: true,
+  },
   quantity: { type: Number, required: true, default: 1 },
 });
 export default mongoose.model("carts", cartSchema);
